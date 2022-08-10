b11e533acb7f39699fa91b09dd1634120236ee01
raffaello64 <108016017+raffaello64@users.noreply.github.com>
2022-08-10 19:59:30 +0200

    Ejercicio de python con información  de persona

---
+++ infopersona.py
@@ -0,0 +1,15 @@
+Nombre = 'Rafael'
+Apellidos = 'Jimenez Cardona'
+Edad = '36'
+Email = 'jimenezrafael@gmail.com'
+Telefono = '99536247'
+Casado = True
+Con_Hijos = False
+Lista_de_amigos = ['Mario', 'Domingo', 'Christian']
+Peliculas_vistas = {
+'Pelicula1' : 'Django', 'Pelicula2' : 'Burning', 'Pelicula3' : 'The Good The Bad and The Ugly'
+}
+print("Nombre: ", Nombre, "\nApellidos: ", Apellidos, "\nEdad: ", Edad,
+ "\nE-mail: ", Email, "\nTelefono: ", Telefono, "\n¿Casado?", Casado,
+  "\n¿Con hijos?", Con_Hijos, "\nLista de amigos: ", Lista_de_amigos,
+   "\nPeliculas vistas: ", Peliculas_vistas, end =" ")
